a17348cdde88a167de07c0d4a9189ace47ceb78c
RaffiRahman <rafirahmanit21@gmail.com>
2025-04-07 12:42:17 +0600

    8th commit

--- mail.py
+++ mail.py
@@ -12,11 +12,12 @@ conf = ConnectionConfig(
     MAIL_USERNAME=config_credentials["EMAIL"],
     MAIL_PASSWORD=config_credentials["PASS"],
     MAIL_FROM=config_credentials["EMAIL"],
-    MAIL_PORT=587,
+    MAIL_PORT=587,  
     MAIL_SERVER="smtp.gmail.com",
     MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
+    MAIL_SSL_TLS=False,  
     USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
 )
 
 
